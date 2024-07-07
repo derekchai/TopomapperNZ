bb23c7da6119a0c9bb1ca911924da6aab2777bac
Derek Chai <chderek9@gmail.com>
2024-07-08 10:10:57 +1200

    Remove auto-generated SwiftData skeleton

--- TopomapperNZ/ContentView.swift
+++ TopomapperNZ/ContentView.swift
@@ -9,9 +9,6 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
         TabView {
             Tab("Routes", systemImage: "point.topleft.down.to.point.bottomright.curvepath") {
@@ -28,24 +25,8 @@ struct ContentView: View {
             
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
