d031eccd2f7367383a47cfa51c1d237b51b3f1d5
Derek Chai <chderek9@gmail.com>
2024-07-08 10:04:44 +1200

    Initial Commit

---
+++ TopomapperNZTests/TopomapperNZTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopomapperNZTests.swift
+//  TopomapperNZTests
+//
+//  Created by Derek Chai on 08/07/2024.
+//
+
+import Testing
+@testable import TopomapperNZ
+
+struct TopomapperNZTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
