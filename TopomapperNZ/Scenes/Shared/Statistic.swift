2802d358e4b06708a5c000a964fbb29782eeadd3
Derek Chai <chderek9@gmail.com>
2024-07-08 11:46:52 +1200

    Add Statistic view

---
+++ TopomapperNZ/Scenes/Shared/Statistic.swift
@@ -0,0 +1,38 @@
+//
+//  Statistic.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 08/07/2024.
+//
+
+import SwiftUI
+
+struct Statistic: View {
+    var label: String
+    var systemImageName: String
+    var value: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.caption)
+                .fontWeight(.bold)
+                .textCase(.uppercase)
+                .foregroundStyle(.secondary)
+            
+            HStack {
+                Image(systemName: systemImageName)
+                    .foregroundStyle(.secondary)
+                
+                Text(value)
+            }
+        }
+    }
+}
+
+
+// MARK: - Preview
+
+#Preview {
+    Statistic(label: "Length", systemImageName: "point.topleft.down.to.point.bottomright.curvepath.fill", value: "74 km")
+}
