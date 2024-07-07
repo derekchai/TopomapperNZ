590feaddcc6e867bd6517290ee33fa859500b374
Derek Chai <chderek9@gmail.com>
2024-07-08 10:14:29 +1200

    Add Symbol enum
    This enum contains the system names of SF Symbols used within Topomapper NZ.

---
+++ TopomapperNZ/Constants/Symbol.swift
@@ -0,0 +1,14 @@
+//
+//  Symbol.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 08/07/2024.
+//
+
+import Foundation
+
+enum Symbol {
+    static let path = "point.topleft.down.to.point.bottomright.curvepath"
+    static let map = "map"
+    static let profile = "person.crop.circle"
+}
