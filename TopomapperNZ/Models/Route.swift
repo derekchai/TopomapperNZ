077d9267f84b40357db06df15900f8c36ee231e4
Derek Chai <chderek9@gmail.com>
2024-07-08 10:57:24 +1200

    Add Route class

---
+++ TopomapperNZ/Models/Route.swift
@@ -0,0 +1,18 @@
+//
+//  Route.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 08/07/2024.
+//
+
+import Foundation
+
+final class Route {
+    var name: String
+    var creationDate: Date
+    
+    init(name: String, creationDate: Date) {
+        self.name = name
+        self.creationDate = creationDate
+    }
+}
