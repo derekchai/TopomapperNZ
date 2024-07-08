0cf4ff055f7b57f09b6e083964434cc2f9ffa067
Derek Chai <chderek9@gmail.com>
2024-07-08 12:30:50 +1200

    Add Location class

---
+++ TopomapperNZ/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 08/07/2024.
+//
+
+import Foundation
+import CoreLocation
+
+class Location {
+    let coordinate: CLLocationCoordinate2D
+    let elevation: CLLocationDistance
+    
+    // MARK: - Initializer
+    init(coordinate: CLLocationCoordinate2D, elevation: CLLocationDistance) {
+        self.coordinate = coordinate
+        self.elevation = elevation
+    }
+}
