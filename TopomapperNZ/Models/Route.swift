8defd5bf549ced7284ed1b076e65461da82580b1
Derek Chai <chderek9@gmail.com>
2024-07-08 18:04:00 +1200

    Make Route a @Model, conform to Codable

--- TopomapperNZ/Models/Route.swift
+++ TopomapperNZ/Models/Route.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+import SwiftData
 
+@Model
 final class Route: Hashable, Codable {
     var name: String
     var creationDate: Date
@@ -17,6 +19,27 @@ final class Route: Hashable, Codable {
         self.name = name
         self.creationDate = creationDate
         self.points = points
+    }
+    
+    enum CodingKeys: CodingKey {
+        case name
+        case creationDate
+        case points
+    }
+    
+    // MARK: - Codable Conformance
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        creationDate = try container.decode(Date.self, forKey: .creationDate)
+        points = try container.decode([RoutePoint].self, forKey: .points)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(creationDate, forKey: .creationDate)
+        try container.encode(points, forKey: .points)
     }
     
     // MARK: - Equatable Conformance
