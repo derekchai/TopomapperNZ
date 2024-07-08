8242b653cc17ec3fb0e6bef69df9c31d3154d293
Derek Chai <chderek9@gmail.com>
2024-07-08 23:05:41 +1200

    Add NewRouteSheet

--- TopomapperNZ/Scenes/Routes/NewRouteSheet+Actions.swift
+++ TopomapperNZ/Scenes/Routes/NewRouteSheet+Actions.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+
+extension NewRouteSheet {
+    func dismissSheet() {
+        dismiss()
+    }
+    
+    func dismissErrorAlert() {
+        self.didError = false
+    }
+    
+    func presentFileImporter() {
+        self.isPresentingFileImporter = true
+    }
+    
+    func handleFileImporterResult(_ result: Result<[URL], Error>) {
+        switch result {
+        case .success(let urls):
+            guard let url = urls.first else { return }
+            
+            let gotAccess = url.startAccessingSecurityScopedResource()
+            
+            guard gotAccess else { return }
+            
+            selectedFileURL = url
+            selectedFileName = url.lastPathComponent
+            
+            url.stopAccessingSecurityScopedResource()
+        case .failure(let error):
+            print("Error: \(error)")
+        }
+    }
+    
+    func saveRoute() {
+        guard let selectedFileURL else {
+            showErrorAlert(title: "This file could not be opened")
+            return
+        }
+        
+        isProcessingGPXFile = true
+        
+        let gpxParser = GPXParser()
+        
+        do {
+            let locations: [Location] = try gpxParser.parsed(at: selectedFileURL)
+            
+            let route = Route(
+                name: routeName,
+                creationDate: Date(),
+                points: [RoutePoint].init(from: locations)
+            )
+            
+            modelContext.insert(route)
+        } catch {
+            showErrorAlert(for: error, title: "This file could not be processed")
+        }
+        
+        isProcessingGPXFile = false
+        dismiss()
+    }
+    
+    // MARK: - Private Functions
+    private func showErrorAlert(for error: Error? = nil, title: String) {
+        self.didError = true
+        self.error = error
+        self.errorAlertTitle = title
+    }
+}
--- TopomapperNZ/Scenes/Routes/NewRouteSheet.swift
+++ TopomapperNZ/Scenes/Routes/NewRouteSheet.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct NewRouteSheet: View {
+    @Environment(\.modelContext) internal var modelContext
+    @Environment(\.dismiss) internal var dismiss
+    
+    @State internal var routeName: String = ""
+    
+    @State internal var selectedFileName: String? = nil
+    @State internal var selectedFileURL: URL? = nil
+    
+    @State internal var isPresentingFileImporter = false
+    @State internal var isPresentingNewRouteSheet = false
+    
+    @State internal var isProcessingGPXFile = false
+    
+    @State internal var didError = false
+    @State internal var error: Error? = nil
+    @State internal var errorAlertTitle = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Group {
+                if isProcessingGPXFile {
+                    ProgressView("Processing file...")
+                } else {
+                    Form {
+                        TextField("Route name", text: $routeName)
+                        
+                        HStack {
+                            Text("GPX file")
+                            
+                            Spacer()
+                            
+                            if let selectedFileName = selectedFileName {
+                                Button(selectedFileName, action: presentFileImporter)
+                            } else {
+                                Button("Import from Files", action: presentFileImporter)
+                            }
+                        }
+                    } // Form
+                }
+            }
+            .navigationTitle("New Route")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", role: .cancel, action: dismissSheet)
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save", action: saveRoute)
+                        .disabled(routeName.isEmpty || selectedFileURL == nil)
+                }
+            }
+        } // NavigationStack
+        .fileImporter(
+            isPresented: $isPresentingFileImporter,
+            allowedContentTypes: [.xml],
+            allowsMultipleSelection: false,
+            onCompletion: handleFileImporterResult,
+            onCancellation: {}
+        )
+        .alert(
+            errorAlertTitle,
+            isPresented: $didError,
+            presenting: error
+        ) { _ in
+            Button("Ok", action: dismissErrorAlert)
+        } message: { error in
+            Text("An error occurred: \(error.localizedDescription)")
+        }
     }
 }
 
