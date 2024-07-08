2058b7cfebe90431dbdfa9fff4cce40b68bc2067
Derek Chai <chderek9@gmail.com>
2024-07-08 12:04:30 +1200

    Add dummy UI to RouteContent

--- TopomapperNZ/Scenes/Routes/RouteContent.swift
+++ TopomapperNZ/Scenes/Routes/RouteContent.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import Charts
 
 struct RouteContent: View {
     @Binding var preferredColumn: NavigationSplitViewColumn
@@ -15,21 +16,96 @@ struct RouteContent: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
+                
+                // MARK: - Map
+                Text("Map")
+                    .font(.headline)
+                
                 Map(interactionModes: []) {}
-                    .frame(height: 400)
-                    .onTapGesture {
-                        preferredColumn = .detail
-                    }
+                    .frame(height: 350)
+                    .onTapGesture(perform: showDetailColumn)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                 
-                Text(route.creationDate.formatted())
-                Text("Route content goes here...")
+                Text("Created on \(route.creationDate.formatted())")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                
+                // MARK: - Statistics
+                Text("Statistics")
+                    .header()
+                
+                HStack {
+                    Statistic(
+                        label: "Length",
+                        systemImageName: Symbol.distance,
+                        value: "38.0 km"
+                    )
+                    Divider()
+                    Statistic(
+                        label: "Alt. Gain",
+                        systemImageName: Symbol.elevationGain,
+                        value: "1,432 m"
+                    )
+                    Divider()
+                    Statistic(
+                        label: "Alt. Loss",
+                        systemImageName: Symbol.elevationLoss,
+                        value: "435 m"
+                    )
+                }
+                
+                // MARK: - Elevation Profile
+                Text("Elevation Profile")
+                    .header()
+                
+                Chart {
+                    LinePlot(x: "x", y: "y = sin(x)") { sin($0) }
+                        .foregroundStyle(by: .value("expression", "y=sin(x)"))
+                        .opacity(0.8)
+                }
+                .chartXScale(domain: -10 ... 10)
+                .chartYScale(domain: -1 ... 1)
+                .frame(height: 200)
+                .onTapGesture(perform: showDetailColumn)
+                
+                // MARK: - Breakdown by Day
+                Text("Breakdown by Day")
+                    .header()
+                
+                Text("Day 1")
+                    .font(.subheadline)
+                Text("Day 2")
+                    .font(.subheadline)
+                Text("Day 3")
+                    .font(.subheadline)
+                
             }
         }
         .padding()
     }
 }
 
+// MARK: - Actions
+extension RouteContent {
+    private func showDetailColumn() {
+        preferredColumn = .detail
+    }
+}
+
+// MARK: - View Extensions
+/// The amount of padding above each section's header (excluding the first
+/// section).
+fileprivate let sectionPadding: CGFloat = 25
+
+fileprivate extension View {
+    func header() -> some View {
+        self
+            .font(.headline)
+            .padding(.top, sectionPadding)
+    }
+}
+
+// MARK: - Preview
 #Preview {
     @Previewable @State var preferredColumn = NavigationSplitViewColumn.content
     
