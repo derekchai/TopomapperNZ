6d697513cd7325cd51ef18a7059b97d0fddd043d
Derek Chai <chderek9@gmail.com>
2024-07-08 22:53:57 +1200

    Add [RoutePoint] initializer from [Location]

--- TopomapperNZ/Extensions/[RoutePoint]+Initializers.swift
+++ TopomapperNZ/Extensions/[RoutePoint]+Initializers.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import CoreLocation
+
+extension [RoutePoint] {
+    init(from locations: [Location]) {
+        var distanceFromStart: CLLocationDistance = 0
+        
+        var previousLocation: Location? = nil
+        
+        var output: [RoutePoint] = []
+        
+        for location in locations {
+            guard let previousLocation else {
+                let routePoint = RoutePoint(
+                    location: location,
+                    distanceFromStart: 0,
+                    grade: 0
+                )
+                
+                output.append(routePoint)
+                
+                previousLocation = location
+                
+                continue
+            }
+            
+            let distanceFromPreviousLocation = location.distance(from: previousLocation)
+            let elevationChangeFromPreviousLocation = location.elevation - previousLocation.elevation
+            
+            distanceFromStart += distanceFromPreviousLocation
+            
+            let grade = elevationChangeFromPreviousLocation / distanceFromPreviousLocation
+            
+            let routePoint = RoutePoint(
+                location: location,
+                distanceFromStart: distanceFromStart,
+                grade: grade
+            )
+            
+            output.append(routePoint)
+        }
+        
+        self.init(output)
+    }
+}
