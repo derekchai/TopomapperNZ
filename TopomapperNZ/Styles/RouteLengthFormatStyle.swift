78a8491eb8ab5a2cd86258bda836b54c4d2b23b2
Derek Chai <chderek9@gmail.com>
2024-07-10 09:24:21 +1200

    Add RouteLengthFormatStyle
    This format style represents a Route's length.

--- TopomapperNZ/Styles/RouteLengthFormatStyle.swift
+++ TopomapperNZ/Styles/RouteLengthFormatStyle.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreLocation
+
+struct RouteLengthFormatStyle: FormatStyle {
+    typealias FormatInput = CLLocationDistance
+    typealias FormatOutput = String
+    
+    func format(_ value: CLLocationDistance) -> String {
+        Measurement(value: value, unit: UnitLength.meters)
+            .formatted(routeLengthFormatStyle)
+    }
+}
+
+extension FormatStyle where Self == RouteLengthFormatStyle {
+    /// A style for formatting the length of a `Route`.
+    static var routeLength: Self { Self() }
+}
+
+private let routeLengthFormatStyle = Measurement<UnitLength>.FormatStyle(
+    width: .abbreviated,
+    usage: .road,
+    numberFormatStyle: .number.precision(.fractionLength(1...1))
+)
