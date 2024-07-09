39ffdf5afd56eb02345b8e35c90433bdd0b6cdc8
Derek Chai <chderek9@gmail.com>
2024-07-10 09:27:26 +1200

    Add ElevationFormatStyle for formatting elevation

---
+++ TopomapperNZ/Styles/ElevationFormatStyle.swift
@@ -0,0 +1,30 @@
+//
+//  ElevationFormatStyle.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 10/07/2024.
+//
+
+import Foundation
+import CoreLocation
+
+struct ElevationFormatStyle: FormatStyle {
+    typealias FormatInput = CLLocationDistance
+    typealias FormatOutput = String
+    
+    func format(_ value: CLLocationDistance) -> String {
+        Measurement(value: value, unit: UnitLength.meters)
+            .formatted(elevationFormatStyle)
+    }
+}
+
+extension FormatStyle where Self == ElevationFormatStyle {
+    /// A style for formatting elevation.
+    static var elevation: Self { Self() }
+}
+
+private let elevationFormatStyle = Measurement<UnitLength>.FormatStyle(
+    width: .abbreviated,
+    usage: .asProvided,
+    numberFormatStyle: .number.precision(.fractionLength(0...0))
+)
