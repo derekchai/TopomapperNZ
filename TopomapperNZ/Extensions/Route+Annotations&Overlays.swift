9be416f09dfcae93f9946c5f9b3f16d0419636b7
Derek Chai <chderek9@gmail.com>
2024-07-10 21:46:56 +1200

    Add startEndAnnotations on Route
    This computed property returns the start and end StartEndAnnotations for the given Route.

--- TopomapperNZ/Extensions/Route+Annotations&Overlays.swift
+++ TopomapperNZ/Extensions/Route+Annotations&Overlays.swift
@@ -26,4 +26,21 @@ extension Route {
         
         return [outlinePolyline, pathPolyline]
     }
+    
+    /// Returns the start and end `StartEndAnnotation`s for this `Route`.
+    var startEndAnnotation: [StartEndAnnotation] {
+        guard !self.points.isEmpty else { return [] }
+        
+        let startAnnotation = StartEndAnnotation(
+            coordinate: self.points.first!.coordinate,
+            pointType: .start
+        )
+        
+        let endAnnotation = StartEndAnnotation(
+            coordinate: self.points.last!.coordinate,
+            pointType: .end
+        )
+        
+        return [startAnnotation, endAnnotation]
+    }
 }
