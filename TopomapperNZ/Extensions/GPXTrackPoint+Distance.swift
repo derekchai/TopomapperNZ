27f0676618488aacff135f9e3535c1725518aa00
Derek Chai <chderek9@gmail.com>
2024-07-11 21:10:34 +1200

    Document distance(from:) on GPXTrackPoint

--- TopomapperNZ/Extensions/GPXTrackPoint+Distance.swift
+++ TopomapperNZ/Extensions/GPXTrackPoint+Distance.swift
@@ -9,6 +9,10 @@ import Foundation
 import CoreLocation
 
 extension GPXTrackPoint {
+    /// Calculates the distance in meters from the given `GPXTrackPpoint`, taking into
+    /// account the curvature of the Earth.
+    /// - Parameter gpxTrackPoint: The point to calculate the distance from.
+    /// - Returns: The distance between the two points, in meters.
     func distance(from gpxTrackPoint: GPXTrackPoint) -> CLLocationDistance {
         let clLocation1 = CLLocation(from: self)
         let clLocation2 = CLLocation(from: gpxTrackPoint)
