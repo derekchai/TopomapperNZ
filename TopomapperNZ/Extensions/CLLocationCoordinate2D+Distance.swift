cc8c1c971c275de23886ddaed114917fd2857968
Derek Chai <chderek9@gmail.com>
2024-07-11 21:09:49 +1200

    Document distance(from:) on CLLocationCoordinate2D

--- TopomapperNZ/Extensions/CLLocationCoordinate2D+Distance.swift
+++ TopomapperNZ/Extensions/CLLocationCoordinate2D+Distance.swift
@@ -9,6 +9,10 @@ import Foundation
 import CoreLocation
 
 extension CLLocationCoordinate2D {
+    /// Calculates the distance in meters from the given coordinate, taking into
+    /// account the curvature of the Earth.
+    /// - Parameter coordinate: The coordinate to calculate the distance from.
+    /// - Returns: The distance between the two coordinates, in meters.
     func distance(from coordinate: CLLocationCoordinate2D) -> CLLocationDistance {
         let location1 = CLLocation(
             latitude: self.latitude,
