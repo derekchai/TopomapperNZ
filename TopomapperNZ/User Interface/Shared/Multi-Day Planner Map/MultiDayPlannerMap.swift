63ddcd0b611fc87a38ee5c786b9ab0a33971d698
Derek Chai <chderek9@gmail.com>
2024-07-12 14:08:31 +1200

    Add MultiDayPlannerMap.swift

---
+++ TopomapperNZ/User Interface/Shared/Multi-Day Planner Map/MultiDayPlannerMap.swift
@@ -0,0 +1,12 @@
+//
+//  MultiDayPlannerMap.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 12/07/2024.
+//
+
+import Foundation
+
+class MultiDayPlannerMap: RouteMapViewController {
+    
+}
