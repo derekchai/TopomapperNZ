17d674be62aa9be89857bdffa37ce7acc853a60f
Derek Chai <chderek9@gmail.com>
2024-07-16 09:11:07 +1200

    Reorganize variable order of Symbol.swift

--- TopomapperNZ/Constants/Symbol.swift
+++ TopomapperNZ/Constants/Symbol.swift
@@ -10,18 +10,25 @@ import Foundation
 enum Symbol {
     static let path = "point.topleft.down.to.point.bottomright.curvepath"
     static let map = "map"
+    
     static let profile = "person.crop.circle"
+    
     static let distance = "arrow.left.and.right"
+    static let distanceFromStart = "arrow.forward.to.line"
+
     static let elevation = "arrowtriangle.up"
     static let elevationGain = "arrow.up.forward"
     static let elevationLoss = "arrow.down.forward"
+    
     static let grade = "angle"
+    
     static let expandHorizontal = "arrow.left.and.line.vertical.and.arrow.right"
     static let contractHorizontal = "arrow.right.and.line.vertical.and.arrow.left"
+    
     static let startPoint = "a.circle.fill"
     static let endPoint = "b.circle.fill"
+    
     static let sortBy = "arrow.up.arrow.down"
     static let add = "plus"
     static let noStops = "flag.slash"
-    static let distanceFromStart = "arrow.forward.to.line"
 }
