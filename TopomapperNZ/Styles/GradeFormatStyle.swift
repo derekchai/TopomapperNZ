9ec2533979ad3800868fd746fa07b7a6d11cff88
Derek Chai <chderek9@gmail.com>
2024-07-15 17:42:50 +1200

    Add .grade FormatStyle for formatting percentage grades

---
+++ TopomapperNZ/Styles/GradeFormatStyle.swift
@@ -0,0 +1,22 @@
+//
+//  GradeFormatStyle.swift
+//  TopomapperNZ
+//
+//  Created by Derek Chai on 15/07/2024.
+//
+
+import Foundation
+
+struct GradeFormatStyle: FormatStyle {
+    typealias FormatInput = Double
+    typealias FormatOutput = String
+
+    func format(_ value: Double) -> String {
+        String(format: "%.1f%", value * 100)
+    }
+}
+
+extension FormatStyle where Self == ElevationFormatStyle {
+    /// A style for formatting grade as a percentage.
+    static var grade: Self { Self() }
+}
